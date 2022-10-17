a00784b0f8beb30618fc5049ad440ef8fe182dd7
Maddi02 <martin.hummel02@icloud.com>
2022-10-17 14:28:30 +0200

    init

---
+++ Landschaftserhaltungs/LandschaftserhaltungsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandschaftserhaltungsApp.swift
+//  Landschaftserhaltungs
+//
+//  Created by Martin Hummel on 17.10.22.
+//
+
+import SwiftUI
+
+@main
+struct LandschaftserhaltungsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
