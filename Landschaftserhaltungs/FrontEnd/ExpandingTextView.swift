39ccb84cb319aa858f87ba21c0421aa13a1174e8
Maddi02 <martin.hummel02@icloud.com>
2022-11-01 13:43:00 +0100

    Created new TextView which grows by size. Leets see if this works as expected

---
+++ Landschaftserhaltungs/FrontEnd/ExpandingTextView.swift
@@ -0,0 +1,20 @@
+//
+//  ExpandingTextView.swift
+//  Landschaftserhaltungs
+//
+//  Created by Martin Hummel on 01.11.22.
+//
+
+import SwiftUI
+
+struct ExpandingTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExpandingTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpandingTextView()
+    }
+}
---
+++ Landschaftserhaltungs/FrontEnd/WrappedTextView.swift
@@ -0,0 +1,8 @@
+//
+//  WrappedTextView.swift
+//  Landschaftserhaltungs
+//
+//  Created by Martin Hummel on 01.11.22.
+//
+
+import Foundation
