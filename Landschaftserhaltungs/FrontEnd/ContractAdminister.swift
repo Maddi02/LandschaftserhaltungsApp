c07050483a97c970bb4a8d8c69b242e000052028
Maddi02 <martin.hummel02@icloud.com>
2022-11-05 17:28:26 +0100

    Added actionsheet. In further it will be used to to chosse which field kind is observed

--- Landschaftserhaltungs/FrontEnd/ContractAdminister.swift
+++ Landschaftserhaltungs/FrontEnd/ContractAdminister.swift
@@ -24,6 +24,9 @@ struct ContractFieldItem
 struct ContractAdminister: View {
     @ObservedObject var dataHandler : DataHandler
     @State var filteredContracts : AppContract
+    @State private var showingOptions = false
+    @State private var selection = "None"
+    
     var body: some View {
         Text("Vetragsübersicht").font(.title2).frame(maxWidth: .infinity, alignment: .leading)
         ContractListItem(firstName: filteredContracts.firstName ?? "Unknown", lastName: filteredContracts.lastName , operationNumber: filteredContracts.operationNumber ?? "Unknown", contractTermination:  filteredContracts.contractTermination?.toString() ?? Date().toString(), endOfContract: filteredContracts.contractTermination?.getEndOfContract(date: filteredContracts.contractTermination ?? Date()) ?? Date().toString() , image: filteredContracts.picture ?? UIImage(imageLiteralResourceName: "HFULogo"), deadline: filteredContracts.deadline?.toString() ?? Date().toString()).frame(maxWidth: .infinity, alignment: .top)
@@ -33,27 +36,43 @@ struct ContractAdminister: View {
         VStack{
             Text("Vetragsflächen:").font(.title2).frame(maxWidth: .infinity, alignment: .leading)
             Button(action: {
+                showingOptions.toggle()
                 addRow()
             }){
                 Image(systemName: "plus")
-            }.frame(maxWidth: .infinity, alignment: .trailing).padding(.trailing,30).padding(.bottom,10)
-            
-            
-           
-    
-        }
-        List()
-        {
-            ForEach(dataHandler.test, id: \.self)
+            }.frame(maxWidth: .infinity, alignment: .trailing).padding(.trailing,30).padding(.bottom,10) .actionSheet(isPresented: $showingOptions) {
+                ActionSheet( title: Text("Select a color"),
+                             buttons: [
+                                .default(Text("Red")) {
+                                    selection = "Red"
+                                },
+                                
+                                    .default(Text("Green")) {
+                                        selection = "Green"
+                                    },
+                                
+                                    .default(Text("Blue")) {
+                                        selection = "Blue"
+                                    },
+                             ]
+                )
+                
+            }
+                
+                
+            }
+            List()
             {
-                filteredContracts in
-                ListItemContractArea()
+                ForEach(dataHandler.test, id: \.self)
+                {
+                    filteredContracts in
+                    ListItemContractArea()
+                }
             }
         }
+        private func addRow() {
+            dataHandler.listItemContractArea.append(ListItemContractArea())
+            print("Size: \(dataHandler.listItemContractArea.count)")
+        }
     }
-    private func addRow() {
-        dataHandler.listItemContractArea.append(ListItemContractArea())
-        print("Size: \(dataHandler.listItemContractArea.count)")
-    }
-    }
-
+    
