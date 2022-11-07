2d07bb94582909a483b3c065ca2464bdd38ac291
Maddi02 <martin.hummel02@icloud.com>
2022-11-07 13:40:54 +0100

    added PlantSpeciesClass

---
+++ Landschaftserhaltungs/BackEnd/PlantSpecies.swift
@@ -0,0 +1,26 @@
+//
+//  PlantSpecies.swift
+//  Landschaftserhaltungs
+//
+//  Created by Martin Hummel on 07.11.22.
+//
+
+import Foundation
+
+
+class PlantSpecies
+
+{
+    private var scientificName : String = ""
+    private var redListBw : Character = " "
+    private var responsibility : Character = " "
+    private var oberReihnArea : Character = " "
+    private var blackForest : Character = " "
+    private var odenWald : Character = " "
+    private var nothernGaelandschaften : Character = " "
+    private var southernGaelandschaften : Character = " "
+    private var schaebischeAlb : Character = " "
+    private var alpenvorland : Character = " "
+    private var germanName : String = " "
+    
+}
