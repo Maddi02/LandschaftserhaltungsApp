d6591efdf97684999afe2b594642ee64ebf8a45c
Maddi02 <martin.hummel02@icloud.com>
2022-11-18 11:01:34 +0100

    Added two new coreDatas

---
+++ Landschaftserhaltungs/BackEnd/DataBase/Profile+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Profile+CoreDataClass.swift
+//  Landschaftserhaltungs
+//
+//  Created by Martin Hummel on 18.11.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Profile)
+public class Profile: NSManagedObject {
+
+}
