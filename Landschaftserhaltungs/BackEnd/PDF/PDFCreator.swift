59e6c89998f744b9b4693701e2b0aa70cefb51d2
Maddi02 <martin.hummel02@icloud.com>
2022-11-27 16:06:07 +0100

    Did some further change in PDF Stuff

--- Landschaftserhaltungs/BackEnd/PDF/PDFCreator.swift
+++ Landschaftserhaltungs/BackEnd/PDF/PDFCreator.swift
@@ -1,5 +1,5 @@
 //
-//  PDFCreator.swift
+//  PDFWEB.swift
 //  Landschaftserhaltungs
 //
 //  Created by Martin Hummel on 26.11.22.
@@ -7,87 +7,49 @@
 
 import Foundation
 import PDFKit
+import TPPDF
+class PDFCreator
+{
+    var  url = URL(string: "")
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
 
-class PDFCreator {
-    
-    private var fieldInfo : FieldInformation
-    private var appContract : AppContract
-    init(fieldInfo : FieldInformation, appContract : AppContract)
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
+
+    func generatePdf()
     {
-        self.fieldInfo = fieldInfo
-        self.appContract = appContract
+        url =  getDocumentsDirectory().appendingPathComponent("awesome.pdf")
+       // var url = URL(string: "file://~/Desktop/awesome.pdf")!
+        let document = PDFDocument(format: .a4)
+        document.add(.contentCenter, text: "Emty pdf.")
+        let generator = PDFCreator(document: document)
+        generator.debug = true
+        url =  try! generator.generateURL(filename: "Example.pdf")
+       // print("Output URL:", url)
+
     }
     
-
-    func createPdf() {
-
-            let outputFileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("yourFileName.pdf")
-            let title = "Your Title\n"
-        let text = String(repeating: appContract.firstName ?? "", count: 1)
-
-            let titleAttributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 1)]
-            let textAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]
-
-            let formattedTitle = NSMutableAttributedString(string: title, attributes: titleAttributes)
-            let formattedText = NSAttributedString(string: text, attributes: textAttributes)
+    
+    func generatePdfAndGetURL() -> URL {
+        url =  getDocumentsDirectory().appendingPathComponent("awesome.pdf")
+       // var url = URL(string: "file://~/Desktop/awesome.pdf")!
+        let document = PDFDocument(format: .a4)
+        document.add(.contentCenter, text: "Emty pdf.")
+        let generator = PDFCreator(document: document)
+        generator.debug = true
+        url =  try! generator.generateURL(filename: "Example.pdf")
+        return url!
+    }
+    
+    func getUrl() -> URL
+    {
+        return url!
+    }
+    
+    
+  
         
-        
-        for i in fieldInfo.pictureArray
-        {
-            let image1Attachment = NSTextAttachment()
-            image1Attachment.image = i.picutre
-            image1Attachment.setImageHeight(height: 50)
-            let image1String = NSAttributedString(attachment: image1Attachment)
-            formattedTitle.append(image1String)
-        }
-           
-        
-        
-
-            formattedTitle.append(formattedText)
-
-            // 1. Create Print Formatter with your text.
-
-            let formatter = UISimpleTextPrintFormatter(attributedText: formattedTitle)
-
-            // 2. Add formatter with pageRender
-
-            let render = UIPrintPageRenderer()
-            render.addPrintFormatter(formatter, startingAtPageAt: 0)
-
-            // 3. Assign paperRect and printableRect
-
-            let page = CGRect(x: 0, y: 0, width: 595.2, height: 841.8) // A4, 72 dpi
-            let printable = page.insetBy(dx: 0, dy: 0)
-
-            render.setValue(NSValue(cgRect: page), forKey: "paperRect")
-            render.setValue(NSValue(cgRect: printable), forKey: "printableRect")
-
-            // 4. Create PDF context and draw
-            let rect = CGRect.zero
-
-            let pdfData = NSMutableData()
-            UIGraphicsBeginPDFContextToData(pdfData, rect, nil)
-
-            for i in 1...render.numberOfPages {
-
-                UIGraphicsBeginPDFPage();
-                let bounds = UIGraphicsGetPDFContextBounds()
-                render.drawPage(at: i - 1, in: bounds)
-            }
-
-            UIGraphicsEndPDFContext();
-
-            // 5. Save PDF file
-
-            do {
-                try pdfData.write(to: outputFileURL, options: .atomic)
-
-                print("wrote PDF file with multiple pages to: \(outputFileURL.path)")
-            } catch {
-
-                 print("Could not create PDF file: \(error.localizedDescription)")
-            }
-        }
-}
-
+    }
