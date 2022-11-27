6936211e34c0faaccea09c536e405b6555c952cc
Maddi02 <martin.hummel02@icloud.com>
2022-11-27 14:11:28 +0100

    Added pdf preview to FFH

--- Landschaftserhaltungs/FrontEnd/ContractManagement/ExportPreviewFFH.swift
+++ Landschaftserhaltungs/FrontEnd/ContractManagement/ExportPreviewFFH.swift
@@ -6,222 +6,36 @@
 //
 
 import SwiftUI
-
+import PDFKit
 
 
 
 struct ExportPreviewFFH: View {
+    private  let pdf = PDFFromGit()
     @StateObject var listEntry: ListEntry
     
     var body: some View {
         
-        
-   
-        
-
-            VStack{
-                
-                VStack{
-                    
-                    
-                    Form
-                    {
-                        Section(header: Text("Datum"))
-                        {
-                            Text(listEntry.infos?.dataOfTaking?.toString() ?? "No Data")
-                        }
-                        
-                        Section(header: Text("Bewirtschaftung"))
-                        {
-                            Text(listEntry.infos?.farming ?? "No Data")
-                            
-                        }
-                        
-                        Section(header: Text("Lage"))
-                        {
-                            Text(listEntry.infos?.position ?? "No Data")
-                        }
-                        
-                        Section(header: Text("Vegetationsbeschreibung"))
-                        {
-                            Text(listEntry.infos?.vegetationDescription ?? "No Data")
-                        }
-                        
-                        Section(header: Text("Blühaspekt"))
-                        {
-                            Text(listEntry.infos?.bloomAspect ?? "No Data")
-                        }
-                        
-                        Section(header: Text("Bewertung Arteninventar"))
-                        {
-                            Text(listEntry.infos?.evaluationSpeciesInventory ?? "No Data")
-                        }
-                        
-                        Section(header: Text("Bewertung Habitatstrukturen"))
-                        {
-                            Text(listEntry.infos?.assessmentHabitatStructure ?? "No Data")
-                        }
-                        Group{
-                            Section(header: Text("Bewertung Beeinträchtigungen"))
-                            {
-                                Text(listEntry.infos?.ratingImpairment ?? "No Data")
-                            }
-                            Section(header: Text("Gesamtbewertung Erhaltungszustand"))
-                            {
-                                Text(listEntry.infos?.overallAssessmentOfTheStateOfPreservation ?? "No Data")
-                            }
-                            Section(header: Text("Schutzstatus"))
-                            {
-                                Text(listEntry.infos?.protectionStatus ?? "No Data")
-                            }
-                            Section(header: Text("Faunistische Beobachtungen"))
-                            {
-                                Text(listEntry.infos?.faunisticObservation ?? "No Data")
-                            }
-                            
-                            Section(header: Text("Vertragsziel erfüllt"))
-                            {
-                                Text(listEntry.infos?.contractTarget ?? "No Data")
-                            }
-                            
-                            Section(header: Text("Anpassung Auflagen"))
-                            {
-                                Text(listEntry.infos?.adaptationEditions ?? "No Data")
-                            }
-                            
-                            Section(header: Text("Weiter Pflegemaßnahme"))
-                            {
-                                Text(listEntry.infos?.furtherMaintenanceMeasures ?? "No Data")
-                                
-                            }
-                            
-                            
-                            Group{
-                                Section(header: Text("Anzahl der Pflanzen Schnellaufnahme"))
-                                {
-                                    Text("\(listEntry.PlantArray.count) Arten" )
-                                    
-                                    
-                                    List{
-                                        
-                                        
-                                        ForEach(listEntry.PlantArray)
-                                        {
-                                            i in
-                                            Text("\(i.scientificName ?? "hhh") (\(i.germanName ?? "hh"))" )
-                                        }
-                                    }
-                                    
-                                }
-                                
-                                Section(header: Text("Anzahl der Pflanzen - Genau Aufnahme"))
-                                {
-                                    Text("\(listEntry.PlantArrayLongTerm.count) Arten" )
-                                    List{
-                                        
-                                        
-                                        ForEach(listEntry.PlantArrayLongTerm)
-                                        {
-                                            i in
-                                            Text("\(i.scientificName ?? "hhh") (\(i.germanName ?? "hh"))" )
-                                        }
-                                    }
-                                    
-                                }
-                                
-                                Section(header: Text("Gesamtanzahl der Pflanzen"))
-                                {
-                                    Text("\(listEntry.PlantArrayLongTerm.count + listEntry.PlantArray.count) Arten" )
-                                    
-                                    List{
-                                        
-                                        
-                                        ForEach(listEntry.PlantArrayLongTerm)
-                                        {
-                                            i in
-                                            Text("\(i.scientificName ?? "hhh") (\(i.germanName ?? "hh"))" )
-                                        }
-                                        ForEach(listEntry.PlantArray)
-                                        {
-                                            i in
-                                            Text("\(i.scientificName ?? "hhh") (\(i.germanName ?? "hh"))" )
-                                        }
-                                        
-                                    }
-                                    
-                                }
-                                Section(header: Text("Gesamtanzahl der Pflanzen in Roter Liste"))
-                                {
-                                    Text(" Keine Ahnung woher ich wisse soll Arten" )
-                                    
-                                    List{
-                                        
-                                    }
-                                    
-                                }
-                                
-                                Section(header: Text("Magerkeitszeiger"))
-                                {
-                                    Text(" Keine Ahnung woher ich wisse soll Arten" )
-                                    
-                                    List{
-                                        
-                                    }
-                                    
-                                }
-                                
-                                Section(header: Text("Beeinträchtigende Arten"))
-                                {
-                                    Text(" Keine Ahnung woher ich wisse soll Arten" )
-                                    
-                                    List{
-                                        
-                                    }
-                                    
-                                }
-                                
-                                
-                                Group{
-                                    Section(header: Text("Aufgenommene Fotos"))
-                                    {
-                                        
-                                        PictureViewExport(fieldInformation: listEntry.infos ?? FieldInformation())
-                                        
-                                    }
-                                    
-                                }
-                                
-                                
-                            }
-                            
-                        }
-                        
-                        Button("Generate PDF")
-                        {
-                            
-                        }
-                    }
-                    
-        
-                }
-                Button(action: { NavigationUtil.popToRootView() }) {
-                    Text("Zurück zum Home Bildschirm")
-                }
-            }.navigationBarBackButtonHidden(true)
+        VStack{
+            PDFKitView(showing: PDFDocument(url: pdf.generatePdfAndGetURL())!)
+            HStack{
             
-        
-        
-        
-        
-        
-        
-        
-       
+                Button {
+                    presentShareSheet()
+                } label: {
+                    Label("Teilen", systemImage: "square.and.arrow.up").frame(maxWidth: .infinity , alignment: .trailing).padding(.trailing , 30)
+                }
+            }
+        }
     }
+
     
-    func a() -> [PicutreList]
-    {
-        print(listEntry.infos?.pictureArray.count as Any)
-        return listEntry.infos?.pictureArray ?? []
+    private func presentShareSheet(){
+        pdf.generatePdf()
+        guard let pdfDoc = PDFDocument(url: pdf.getUrl())?.documentURL else {return}
+        let shareSheetVC = UIActivityViewController(activityItems: [pdfDoc], applicationActivities:  [])
+        UIApplication.shared.windows.first?.rootViewController?.present(shareSheetVC, animated: true, completion: nil)
     }
 }
+
+
