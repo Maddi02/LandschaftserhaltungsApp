9bb533dcb6077326474db6326d0cea13d95f5cff
biehm <marc.biehler@hs-furtwangen.de>
2022-12-04 14:33:27 +0100

    Added CSV FeatureOverview and changed SecondFeatureOverview

--- Landschaftserhaltungs/FrontEnd/Onboarding/SecondFeatureOverview.swift
+++ Landschaftserhaltungs/FrontEnd/Onboarding/SecondFeatureOverview.swift
@@ -1,5 +1,6 @@
+
 //
-//  SecondFeatureOverview.swift
+//  FirstFeatureOverview.swift
 //  Landschaftserhaltungs
 //
 //  Created by Martin Hummel on 17.10.22.
@@ -8,28 +9,9 @@
 import SwiftUI
 
 struct SecondFeatureOverview: View {
-    var csvGenerator = CSVGenerator()
-    @State var link = URL(string: "https://www.hackingwithswift.com")!
-    
     var body: some View {
-        
-        VStack{
-            Text("Generieren Sie hier ihren App Ordner in welchen Sie die csv Importieren können")
-            
-            Button("generieren"){
-                link = csvGenerator.generateInitFolder()
-                presentShareSheet()
-            }
-        }
-        
-       
-
-        
+        Text("\"Mein Profil\" \n \n Wählen Sie die Sprache der Pflanzennamen.\n").multilineTextAlignment(.center)
     }
-    private func presentShareSheet(){
-            let shareSheetVC = UIActivityViewController(activityItems: [link], applicationActivities:  [])
-            UIApplication.shared.windows.first?.rootViewController?.present(shareSheetVC, animated: true, completion: nil)
-        }
 }
 
 struct SecondFeatureOverview_Previews: PreviewProvider {
