2e89b0434dadc4f8c69103631a8b1f9783022757
biehm <marc.biehler@hs-furtwangen.de>
2022-12-19 15:06:18 +0100

    New NewContract page

---
+++ Landschaftserhaltungs/FrontEnd/Onboarding/NewContract.swift
@@ -0,0 +1,38 @@
+//
+//  NewContract.swift
+//  Landschaftserhaltungs
+//
+//  Created by Marc Biehler on 19.12.22.
+//
+
+import SwiftUI
+
+struct NewContract: View {
+    var body: some View {
+        VStack {
+            Group{
+                Text("Profil anpassen\n")
+                    .multilineTextAlignment(.center)
+            }
+            Group{
+                Text("Im Hauptmenü können Sie über das Profil Icon Ihren Namen und das Logo anpassen (Der Name und das Logo werden im PDF-Bericht angezeigt).\n")
+                    .multilineTextAlignment(.center)
+            }
+            Group{
+                Text("Außerdem können hier noch die Sprache der Pflanzennamen, die in der Artenliste angezeigt werden sollen, und die Qualität der in der App gespeicherten Bilder angepasst werden.\n")
+                    .multilineTextAlignment(.center)
+            }
+            Group{
+                Text("Über “CSV auswählen” können Sie eine Artenliste importieren, die für die Artenzählung genutzt wird (Wenn keine CSV-Datei ausgewählt wird, funktioniert die Artenzählung u.a. nicht).\n")
+                    .multilineTextAlignment(.center)
+            }
+        }
+    }
+}
+
+struct NewContract_Previews: PreviewProvider {
+    static var previews: some View {
+        NewContract()
+    }
+}
+
