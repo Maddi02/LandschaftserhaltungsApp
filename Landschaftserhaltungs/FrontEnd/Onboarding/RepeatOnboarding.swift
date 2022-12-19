d80811adc1d93ef37d61aff6f65bd0c57f566855
biehm <marc.biehler@hs-furtwangen.de>
2022-12-19 15:17:10 +0100

    New RepeatOnboarding page

--- Landschaftserhaltungs/FrontEnd/Onboarding/RepeatOnboarding.swift
+++ Landschaftserhaltungs/FrontEnd/Onboarding/RepeatOnboarding.swift
@@ -11,23 +11,7 @@ struct RepeatOnboarding: View {
     var body: some View {
         VStack {
             Group{
-                Text("Vertragsübersicht\n")
-                    .multilineTextAlignment(.center)
-            }
-            Group{
-                Text("Über das + können Sie dem Vertrag eine Teilfläche hinzufügen. Dabei können Sie einen Namen,die Art der Teilfläche und das Datum der Artenzählung wählen. Die Teilflächen können ebenfalls durch Wischen nach links gelöscht werden.\n")
-                    .multilineTextAlignment(.center)
-            }
-            Group{
-                Text("Für jede Teilfläche können Sie eine Artenzählung und eine Häufigkeitsschätzung durchführen und die Ergebnisse anschließend als PDF-Datei (Bericht) oder CSV-Datei exportieren.\n")
-                    .multilineTextAlignment(.center)
-            }
-            Group{
-                Text("Hinweis:")
-                    .multilineTextAlignment(.center)
-            }
-            Group{
-                Text("Die in der Artenzählung eingegebene Information kann später noch angepasst werden.\n")
+                Text("Das Onboarding kann über das Profil Icon im Hauptmenü wiederholt werden.\n")
                     .multilineTextAlignment(.center)
             }
         }
