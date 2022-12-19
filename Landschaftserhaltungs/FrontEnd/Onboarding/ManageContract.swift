d3bcb0b4eccad214294990735f549946128e299b
biehm <marc.biehler@hs-furtwangen.de>
2022-12-19 15:08:55 +0100

    New ManageContract page

---
+++ Landschaftserhaltungs/FrontEnd/Onboarding/ManageContract.swift
@@ -0,0 +1,39 @@
+//
+//  ManageContract.swift
+//  Landschaftserhaltungs
+//
+//  Created by Marc Biehler on 19.12.22.
+//
+
+import SwiftUI
+
+struct ManageContract: View {
+    var body: some View {
+        VStack {
+            Group{
+                Text("Neuen Vertrag anlegen\n")
+                    .multilineTextAlignment(.center)
+            }
+            Group{
+                Text("Hier können Sie neue Verträge anlegen und in weiteren Schritten Teilflächen anlegen und Artenzählungen und Häufigkeitsschätzungen durchführen.\n")
+                    .multilineTextAlignment(.center)
+            }
+            Group{
+                Text("Die Informationen müssen nicht eingegeben werden, aber es empfiehlt sich, da es die Vertragsverwaltung übersichtlich hält.\n")
+                    .multilineTextAlignment(.center)
+            }
+            Group{
+                Text("Außerdem kann hier eine Übersichtskarte der Vertragsfläche eingefügt werden.\n")
+                    .multilineTextAlignment(.center)
+            }
+        }
+    }
+}
+
+struct ManageContract_Previews: PreviewProvider {
+    static var previews: some View {
+        ManageContract()
+    }
+}
+
+
